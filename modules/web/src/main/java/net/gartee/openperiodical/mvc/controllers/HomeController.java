a02a475114dc9f74d0672679426e7b0bf71dc747
Brett Lilley <blilley@pillartechnology.com>
2015-06-23 12:45:22 -0400

    Created Frontpage with JSP and Thymeleaf

--- modules/web/src/main/java/net/gartee/openperiodical/mvc/controllers/HomeController.java
+++ modules/web/src/main/java/net/gartee/openperiodical/mvc/controllers/HomeController.java
@@ -2,12 +2,18 @@ package net.gartee.openperiodical.mvc.controllers;
 
 import net.gartee.cqrs.CommandHandler;
 import net.gartee.openperiodical.core.commands.CreateNewspaper;
+import net.gartee.openperiodical.core.entities.Newspaper;
 import net.gartee.openperiodical.core.identities.PeriodicalId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -21,16 +27,26 @@ public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String index(ModelMap model) {
+        List<Newspaper> newspapers = new ArrayList();
+        model.addAttribute("newspapers", newspapers);
+        return "index"; // view name (jsp)
+    }
 
-        model.addAttribute("name", "you");
-
-        CreateNewspaper command = new CreateNewspaper(
-                new PeriodicalId(7),
-                "Web Newspaper"
-        );
+    @RequestMapping(value = "Create", method = RequestMethod.POST)
+    public String createNewsPaper(@RequestParam String title)
+    {
+        CreateNewspaper command = new CreateNewspaper(new PeriodicalId(2), title);
 
         createNewspaperCommandHandler.handle(command);
 
-        return "index"; // view name (jsp)
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "Delete/{id}", method = RequestMethod.DELETE)
+    public String deleteNewspaper(@PathVariable("id") int id)
+    {
+        //TODO: Call Delete handler?
+
+        return "redirect:/";
     }
 }
